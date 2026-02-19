c17a8f6277ed832c291cff0ac90ab35206e35bd5
Mirzobek Ruzmetov <ruzmetovmirzobek10@gmail.com>
2026-02-19 21:14:34 +0500

    app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+let ism = "Mirzobek";
+let yosh = 16;
+
+console.log("Salom " + ism);
+console.log("Yosh: " + yosh);
+
+let a = 5;
+let b = 3;
+
+let yigindi = a + b;
+console.log("Yig'indi: " + yigindi);
+
+function salom() {
+  console.log("Xush kelibsiz!");
+}
+
+salom();
